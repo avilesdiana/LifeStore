3f92e763251cf4309af838e60ccfe726b554b0f3
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-01-29 08:55:00 +0000

    Prueba 1 finalizada

--- p.py
+++ p.py
@@ -8,34 +8,32 @@ from lifestore_file import lifestore_products, lifestore_sales, lifestore_search
 #print(lifestore_products[0][0])
 
 top = []
-top1 = []
-arrayto= []
 
 new_lists = sorted(lifestore_sales, key=itemgetter(2), reverse = True)
-print(len(new_lists))
+#print(len(new_lists))
 
 
 i = 1 #columna que queremos obtener
 columna = [fila[i] for fila in new_lists]
-print(columna)
-print(len(columna))
+#print(columna)
+#print(len(columna))
 #columna1 = columna.sort()
 
 for x in range(len(lifestore_products)+1):
   l = columna.count(x)
   top.append(l)
-  print(str(x) + "  " + str(top[x]))
+  #print(str(x) + "  " + str(top[x]))
 
 a = np.array(top)
-print(a)
+#print(a)
 
 indices = np.sort(a)[::-1]
-print(indices)
+#print(indices)
 
 indices1 = np.argsort(a)[::-1]
-print(indices1)
+#print(indices1)
 
-print(indices1[0:5])
+#print(indices1[0:5])
 
 columna3 = indices1[0:5]
 
