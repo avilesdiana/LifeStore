0b37e6ad60dcb0f64c8053e196643c17a007fc18
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-02-01 22:02:51 +0000

    Punto 2 Terminado - Agregarlo al menú

--- p5.py
+++ p5.py
@@ -13,14 +13,109 @@ from operator import itemgetter, attrgetter
 from lifestore_file import lifestore_products, lifestore_sales, lifestore_searches
 
 
+def merge(list1, list2): 
+      
+    merged_list = [(list1[i], list2[i]) for i in range(0, len(list1))] 
+    return merged_list 
+      
+
 validation_refund = []
 
- #Agregamos en una nueva lista los productos de ventas que no tuvieron reembolso == 0 
+ ### lista productos
+
+i = 1 #columna que queremos obtener
+column_products1 = [fila[i] for fila in lifestore_products] 
+
+for x in column_products1:
+  print(x)
+
+i = 0 #columna que queremos obtener
+column_products0 = [fila[i] for fila in lifestore_products] 
+
+for x in column_products0:
+  print(x)
+
+productos_id_name = merge(column_products0 ,column_products1)
+
+for x in productos_id_name :
+  print(x)
+
+  ########
+
+  #Agregamos en una nueva lista solamento los productos que tienen reseña
 for sale0 in range(len(lifestore_sales)):
- if lifestore_sales[sale0][4] == 0:
+ if lifestore_sales[sale0][2] >= 1:
    validation_refund.append(lifestore_sales[sale0])
-   #print(lifestore_sales[sale0]) 
 
 for x in validation_refund:
   print(x)
 
+print(len(validation_refund))
+print(len(lifestore_sales))
+
+i = 1 #columna que queremos obtener
+column_sales0 = [fila[i] for fila in lifestore_sales] 
+
+for x in column_sales0:
+  print(x)
+
+i = 2 #columna que queremos obtener
+column_sales1 = [fila[i] for fila in lifestore_sales] 
+
+for x in column_sales1:
+  print(x)
+
+
+sales_id_name = merge(column_sales0 ,column_sales1)
+
+for x in sales_id_name :
+  print(x)
+
+
+ordenados = sorted(sales_id_name, key=lambda score : score[1], reverse = True)
+
+print("ordenados")
+for x in ordenados:
+  print(x)
+
+resultantList = []
+ 
+for element in ordenados:
+    if element not in resultantList:
+        resultantList.append(element)
+
+
+print("PRODUCTOS FINALES")
+for x in resultantList:
+  print(x)
+
+
+
+
+print("prubeas")
+print(resultantList[0][0])
+print(productos_id_name[0][0])
+
+product_categories20 = []
+
+i = 0 #columna que queremos obtener
+column_sales11 = [fila[i] for fila in resultantList] 
+column_sale12 = column_sales11[0:5]
+print("column_sales11")
+for x in column_sale12:
+  print(x)
+
+print("fin")
+
+
+for column in range(len(productos_id_name)):
+  for row in range(len(column_sale12 )):
+   if productos_id_name[column][0] == column_sale12[row]: 
+    product_categories20.append(productos_id_name[column][0:2])
+
+print("idProducto_SalesXProduct")
+for x in product_categories20:
+  print(x)
+
+
+  
